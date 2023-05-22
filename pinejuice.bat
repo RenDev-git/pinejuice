0272c3b98b77e7ff140eeb9dfbff15ac754fef16
RenDev <99676641+RenDev-git@users.noreply.github.com>
2023-05-23 00:28:13 +0300

    added pinejuice.bat

---
+++ pinejuice.bat
@@ -0,0 +1,9 @@
+@echo off
+cls
+echo pineJuice By rendev 
+echo Download The latest Yuzu Early Access Versions in the fastest Way
+echo . . . 
+py app.py
+echo . . .
+echo Thanks For using pineJuice
+pause
